9ff1985ccd7f801da9633eb3970faa6ab2baa82e
Bruna Drago <bruna_drago@hotmail.com>
2025-10-04 18:59:39 -0300

    [CDN]Make model properties private and add getter and setters

--- CadastroNinjas/src/main/java/dev/bdrago/CadastroNinjas/NinjaModel.java
+++ CadastroNinjas/src/main/java/dev/bdrago/CadastroNinjas/NinjaModel.java
@@ -8,10 +8,10 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    Integer idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private Integer idade;
 
     public NinjaModel() {
 
@@ -20,5 +20,22 @@ public class NinjaModel {
         this.nome = nome;
         this.email = email;
         this.idade = idade;
+    }
+    public Long getId() {
+        return id;
+    }
+ 
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
